458df96623dfcaf03d6ce7ce0bd5dcb292651b3a
LebrEars <seiberlich@posteo.de>
2016-05-26 19:17:28 +0200

    first commit

---
+++ sine.c
@@ -0,0 +1,47 @@
+/*********************************************************************
+*Entwickeln Sie ein Programm, das Ihnen die Werte der Sinusfunktion
+*in 10er Schritten von 0 bis 360° mit drei Stellen nach dem Komma 
+*ausgibt. Die Sinusfunktion sin() ist in der Header-Datei math.h 
+*definiert. Achten Sie auf eventuelle Typkonvertierungen
+*********************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+double zehnSine(void);
+
+int main(void) {
+	
+	// Funktion ausführen
+	zehnSine();
+	
+}
+
+
+// Funktionsdefinition
+double zehnSine(void) {
+	// Konstante Pi definieren
+	const double Pi = 3.141592653;
+	
+	// Variablen definieren
+	double winkel;
+	double rad;
+	double sinus;
+	int i;
+	
+	printf("winkel   sinus");
+
+	for (i = 0; i <= 36; i++) {
+		winkel = i * 10;
+
+		rad = winkel * Pi / 180;
+		sinus = sin(rad);
+
+		printf("%6g  %6.3f\n", winkel, sinus);
+	}
+	return 0;
+}
+
+
+
+
