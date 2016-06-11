0c5f8bcffc7301ba1fef4160eadad145f166fdfd
LebrEars <seiberlich@posteo.de>
2016-06-11 17:27:04 +0200

    now creats sinusCFFI.c to be imported in sinusCFFI.py

--- sineFFI.py
+++ sineFFI.py
@@ -2,33 +2,27 @@ from cffi import FFI
 
 ffi = FFI()
 
-ffi.cdef("double mySine(double winkel);")
-
 ffi.set_source("sinusCFFI",
 """
 #include <stdio.h>
 #include <math.h>
 
 
-double mySine(double winkel) {
-     // Konstante Pi definieren
-    const double Pi = 3.141592653;
+        double mySine(double winkel) {
+             // Konstante Pi definieren
+             const double Pi = 3.141592653;
 
-    // Variablen definieren
-    double rad;
-    double sinus;
+             // Variablen definieren
+             double rad;
+             double sinus;
 
-    rad = winkel * Pi / 180;
-    sinus = sin(rad);
-    return sinus;
-}
+             rad = winkel * Pi / 180;
+             sinus = sin(rad);
+             return(sinus);
+        }
 """)
 
+ffi.cdef("double mySine(double winkel);")
+
 ffi.compile()
-
-
-lib = ffi.dlopen('sinusCFFI')
-s = lib.mySine(ffi.new("double w", "20"))
-
-print(s)
 
